215a4d0c135f5c2f0b0e0ef8896ea82a3780e3be
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-04 16:22:12 +0530

    cleared existing style from App.css

---
+++ contact-us/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+        <img src="../assets/logo.jpg" alt="logo" height="50px" width="50px" />
+        </div>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
