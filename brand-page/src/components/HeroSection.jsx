4ae3dda0be7c2b167a3207fe814b7579877062cd
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-04 15:21:07 +0530

    completed HeroSecton component

--- brand-page/src/components/HeroSection.jsx
+++ brand-page/src/components/HeroSection.jsx
@@ -1,5 +1,23 @@
 const HeroSection = () => {
-  return <div>Herosection</div>;
+  return (
+    <main>
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVES THE BEST</h1>
+        <p>
+          YOUR FEET DESERVES THE BEST AND WE ARE HERE TO HELP YOU WITH OUR
+          SHOES. YOUR FEET DESERVES THE BEST AND WE ARE HERE TO HELP YOU WITH
+          OUR SHOES.
+        </p>
+        <div>
+          <button>Shop Now</button>
+          <button id="category">Category</button>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="src/assets/heroimage.jpg" alt="sneakers" height="auto" width="300px"/>
+      </div>
+    </main>
+  );
 };
 
 export default HeroSection;
