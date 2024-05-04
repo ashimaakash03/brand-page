532987bdbef823611314ae0ee2044d9f0797348d
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-04 14:36:14 +0530

    moved navbar code to Navigation component

--- brand-page/src/App.jsx
+++ brand-page/src/App.jsx
@@ -3,23 +3,7 @@ import "./App.css";
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img
-            src="src/assets/logo.PNG"
-            alt="logo"
-            height="50px"
-            width="100px"
-          />
-        </div>
-        <ul>
-          <li href="#">MENU</li>
-          <li href="#">LOCATION</li>
-          <li href="#">ABOUT</li>
-          <li href="#">CONTACT</li>
-        </ul>
-        <button>Login</button>
-      </nav>
+    
     </div>
   );
 };
--- brand-page/src/components/Navigation.jsx
+++ brand-page/src/components/Navigation.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
 const Navigation = () => {
   return (
     <div>
-      
+      <nav>
+        <div className="logo">
+          <img
+            src="src/assets/logo.PNG"
+            alt="logo"
+            height="50px"
+            width="100px"
+          />
+        </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>Login</button>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
