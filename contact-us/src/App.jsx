9901a97f79f2370a82a05c8eec05e5e57ba1ad2b
713068_Ashim <ashimaakash1997@gmail.com>
2024-05-04 17:10:46 +0530

    displayed contact component

--- contact-us/src/App.jsx
+++ contact-us/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Contact from "./components/Contact";
 import Navigation from "./components/Navigation";
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Contact />
     </div>
   );
 };
--- contact-us/src/components/Contact.jsx
+++ contact-us/src/components/Contact.jsx
@@ -1,9 +1,15 @@
 const Contact = () => {
   return (
-    <div>
-      
+    <div className="contact">
+      <h1>CONTACT US...</h1>
+      <p>
+        LET US CONNECT: WE ARE HERE TO HELP, AND WE WOULD LOVE TO HELP. WHETHER
+        YOU HAVE A QUESTION, COMMENT OR JUST WANT TO CHAT, YOU CAN REACH OUT TO
+        US THROUGH THE CONTACT FORM, OF THIS PAGE, OR THROUGH PHONE, EMAIL OR
+        SOCIAL MEDIA.{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
